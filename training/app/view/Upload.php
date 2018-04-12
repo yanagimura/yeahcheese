ccb6c452173b465a85db047b6bfe8c02b4325ede
Mai Yanagimura <yanagimura@sencorp.co.jp>
2018-04-12 19:21:30 +0900

    作成イベントアップロード処理のアクションとビュークラス及びテンプレートのスケルトン生成

---
+++ training/app/view/Upload.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  Upload.php
+ *
+ *  @author     {$author}
+ *  @package    Sharepictures
+ */
+
+/**
+ *  upload view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sharepictures
+ */
+class Sharepictures_View_Upload extends Sharepictures_ViewClass
+{
+    /**
+     *  セッション'create'からデータを取得後、upload.tplに反映
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+    }
+}
