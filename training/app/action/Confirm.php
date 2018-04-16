a6812cbbcba7e293bc0bc72c00b8ef449cafce55
Mai Yanagimura <yanagimura@sencorp.co.jp>
2018-04-16 16:55:55 +0900

    eventsテーブルとpicturesテーブルへの書き込み処理の追加

--- training/app/action/Confirm.php
+++ training/app/action/Confirm.php
@@ -5,7 +5,7 @@
  *  @author     {$author}
  *  @package    Sharepictures
  */
-
+require_once('adodb5/adodb.inc.php');
 /**
  *  confirm Form implementation.
  *
@@ -46,14 +46,37 @@ class Sharepictures_Action_Confirm extends Sharepictures_ActionClass
     }
 
     /**
-     *  データベースに書き込み、
-     *  セッション'create'に認証キーを追加、upload.tplを表示
+     *  セッション'create'に認証キーを追加、データベースに書き込み、
+     *  DBupload.tplを表示
      *  @access public
      *  @return string  forward name.
      */
     public function perform()
     {
-        return 'confirm';
+        $str = array_merge(range('a','z'), range('0', '9'), range('A', 'Z'));
+        $authkey = null;
+        for ($i = 0; $i < 6; $i++) {
+            $authkey .= $str[rand(0, count($str) - 1)];
+        }
+
+        $db = $this->backend->getDB();
+
+        // eventのレコードを追加する
+        $sql = "INSERT INTO events(title, user_id, release_date, end_date, authentication_key) VALUES($1, $2, $3, $4, $5)";
+        $db->query($sql, [$this->session->get('create')['title'], $this->session->get('login')['id'],
+          $this->session->get('create')['release_date'], $this->session->get('create')['end_date'], $authkey]);
+
+        // 最後に追加されたレコードを取得する
+        $sql = "SELECT * FROM events ORDER BY id DESC LIMIT 1";
+        $eventID = $db->getRow($sql)['id'];
+
+        //  picturesのレコードを追加する
+        $sql = "INSERT INTO pictures(filename, event_id) VALUES($1, $2)";
+        foreach($this->session->get('create')['picture_array'] as $filename){
+          $db->query($sql, [$filename, $eventID]);
+        }
+
+        return 'upload';
     }
 
     /**
