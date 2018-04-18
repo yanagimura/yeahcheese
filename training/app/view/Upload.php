8122e391174594ec53e4dc7f8bcea738bc162e5c
Mai Yanagimura <yanagimura@sencorp.co.jp>
2018-04-18 13:37:01 +0900

    ビュークラス内のセッション処理の修正

--- training/app/view/Upload.php
+++ training/app/view/Upload.php
@@ -27,8 +27,7 @@ class Sharepictures_View_Upload extends Sharepictures_ViewClass
         $this->af->setApp('release_date', $this->session->get('create')['release_date']);
         $this->af->setApp('end_date', $this->session->get('create')['end_date']);
         $this->af->setApp('count', count($this->session->get('create')['picture_array']));
-
-        unset($_SESSION['create']);
-        unset($_SESSION['authSession']);
+        $this->session->remove('create');
+        $this->session->remove('authSession');
     }
 }
