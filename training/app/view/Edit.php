57583b8ac8eb438d3223eb33261ddf5fb0d3e61e
Mai Yanagimura <yanagimura@sencorp.co.jp>
2018-04-18 12:01:20 +0900

    テンプレートにフォーム値を渡す処理の実装

--- training/app/view/Edit.php
+++ training/app/view/Edit.php
@@ -22,9 +22,11 @@ class Sharepictures_View_Edit extends Sharepictures_ViewClass
      */
     public function preforward()
     {
-        $this->af->setApp('title', $this->session->get('edit')['title']);
-        $this->af->setApp('release_date', $this->session->get('edit')['release_date']);
-        $this->af->setApp('end_date', $this->session->get('edit')['end_date']);
+        $this->af->form['title']['default'] = $this->session->get('edit')['title'];
+        $this->af->form['release_date']['default'] = $this->session->get('edit')['release_date'];
+        $this->af->form['end_date']['default'] = $this->session->get('edit')['end_date'];
         $this->af->setApp('authentication_key', $this->session->get('edit')['authentication_key']);
+        $this->af->setApp('picture_array', $this->session->get('edit')['picture_array']);
+
     }
 }
