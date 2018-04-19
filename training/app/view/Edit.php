dafec5cab148d08c661967a6435b45f6f3c4579d
Mai Yanagimura <yanagimura@sencorp.co.jp>
2018-04-19 16:24:43 +0900

    編集画面のビュークラスの実装

---
+++ training/app/view/Edit.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *  Edit.php
+ *
+ *  @author     {$author}
+ *  @package    Sharepictures
+ */
+
+/**
+ *  edit view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sharepictures
+ */
+class Sharepictures_View_Edit extends Sharepictures_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+        if ($this->af->form['title']['default'] === null) {
+            $this->af->form['title']['default'] = $this->session->get('edit')['title'];
+            $this->af->form['release_date']['default'] = $this->session->get('edit')['release_date'];
+            $this->af->form['end_date']['default'] = $this->session->get('edit')['end_date'];
+          }
+
+        $this->af->setApp('authentication_key', $this->session->get('edit')['authentication_key']);
+        $this->af->setApp('picture_array', $this->session->get('edit')['picture_array']);
+    }
+}
