840eb085e1a01933af03350fd5650102e8604caf
Mai Yanagimura <yanagimura@sencorp.co.jp>
2018-04-19 16:17:08 +0900

    編集画面のビュークラスのスケルトンの実装

---
+++ training/app/action/Edit.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ *  Edit.php
+ *
+ *  @author     {$author}
+ *  @package    Sharepictures
+ */
+
+/**
+ *  edit Form implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sharepictures
+ */
+class Sharepictures_Form_Edit extends Sharepictures_ActionForm
+{
+    /**
+     *  @access protected
+     *  @var    array   form definition.
+     */
+    public $form = array(
+       /*
+        *  TODO: Write form definition which this action uses.
+        *  @see http://ethna.jp/ethna-document-dev_guide-form.html
+        *
+        *  Example(You can omit all elements except for "type" one) :
+        *
+        *  'sample' => array(
+        *      // Form definition
+        *      'type'        => VAR_TYPE_INT,    // Input type
+        *      'form_type'   => FORM_TYPE_TEXT,  // Form type
+        *      'name'        => 'Sample',        // Display name
+        *
+        *      //  Validator (executes Validator by written order.)
+        *      'required'    => true,            // Required Option(true/false)
+        *      'min'         => null,            // Minimum value
+        *      'max'         => null,            // Maximum value
+        *      'regexp'      => null,            // String by Regexp
+        *
+        *      //  Filter
+        *      'filter'      => 'sample',        // Optional Input filter to convert input
+        *      'custom'      => null,            // Optional method name which
+        *                                        // is defined in this(parent) class.
+        *  ),
+        */
+    );
+
+    /**
+     *  Form input value convert filter : sample
+     *
+     *  @access protected
+     *  @param  mixed   $value  Form Input Value
+     *  @return mixed           Converted result.
+     */
+    /*
+    protected function _filter_sample($value)
+    {
+        //  convert to upper case.
+        return strtoupper($value);
+    }
+    */
+}
+
+/**
+ *  edit action implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sharepictures
+ */
+class Sharepictures_Action_Edit extends Sharepictures_ActionClass
+{
+    /**
+     *  preprocess of edit Action.
+     *
+     *  @access public
+     *  @return string    forward name(null: success.
+     *                                false: in case you want to exit.)
+     */
+    public function prepare()
+    {
+    
+        return null;
+    }
+
+    /**
+     *  edit action implementation.
+     *
+     *  @access public
+     *  @return string  forward name.
+     */
+    public function perform()
+    {
+        return 'edit';
+    }
+}
