9a4ebefeed2c5986b1758b094222d2155abb2328
Mai Yanagimura <yanagimura@sencorp.co.jp>
2018-04-19 16:11:18 +0900

    一覧画面のビュークラスの実装

---
+++ training/app/view/Show.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Show.php
+ *
+ *  @author     {$author}
+ *  @package    Sharepictures
+ */
+
+/**
+ *  show view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sharepictures
+ */
+class Sharepictures_View_Show extends Sharepictures_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+        $this->af->setApp('event_array', $this->session->get('show'));
+    }
+}
