42d4e1acc1270e4fc805ef159049dab649a05b84
Mai Yanagimura <yanagimura@sencorp.co.jp>
2018-04-19 12:32:50 +0900

    フォームのデフォルト値設定処理の修正

--- training/app/view/Edit.php
+++ training/app/view/Edit.php
@@ -22,9 +22,11 @@ class Sharepictures_View_Edit extends Sharepictures_ViewClass
      */
     public function preforward()
     {
-        $this->af->form['title']['default'] = $this->session->get('edit')['title'];
-        $this->af->form['release_date']['default'] = $this->session->get('edit')['release_date'];
-        $this->af->form['end_date']['default'] = $this->session->get('edit')['end_date'];
+        if ($this->af->form['title']['default'] === null) {
+            $this->af->form['title']['default'] = $this->session->get('edit')['title'];
+            $this->af->form['release_date']['default'] = $this->session->get('edit')['release_date'];
+            $this->af->form['end_date']['default'] = $this->session->get('edit')['end_date'];
+        }
 
         $this->af->setApp('authentication_key', $this->session->get('edit')['authentication_key']);
         $this->af->setApp('picture_array', $this->session->get('edit')['picture_array']);
